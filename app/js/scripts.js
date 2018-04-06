9be0ec7adfc6436ceba1ef133fc84f5f21613a3d
alexrewrew <alexrewrew@live.com>
2018-04-06 10:20:48 +0300

    Update 6.04

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -11,18 +11,24 @@
         });
 
 
-        // ========== COLOR THIEF ==========
 
-        // $('.slide img').click(function () {
-        //     var imgSrc = $(this).attr('src');
-        //     var imgObj = new Image();
-        //     imgObj.src = imgSrc;
+        // for (var i = 0; i < tabsArr.length; i++) {
         //
-        //     var colorThief = new ColorThief();
-        //     var color = colorThief.getColor(imgObj);
+        //     // var tabsWidth = $(tabsArr[i]).outerWidth();
         //
-        //     $('.box').css('background-color', 'rgb(' + color +')')
-        // })
+        //     $(tabsArr[i]).click(function (e) {
+        //         e.preventDefault();
+        //
+        //         var tabsIndex = $(this).index();
+        //
+        //         console.log(tabsArr);
+        //         console.log(tabsIndex);
+        //
+        //         $(tabsArr).removeClass('active');
+        //         $(this).addClass('active');
+        //
+        //     });
+        // }
 
         // ========== SELECT ==========
 
@@ -43,7 +49,41 @@
         // ========== SLIDER ==========
 
         var arrowLeft = "<div class='arrow left'><svg width=\"27px\" height=\"16px\" viewBox=\"0 0 27 16\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\"> <g id=\"Pre-fin-Copy-2\" transform=\"translate(-592.000000, -721.000000)\" fill=\"#FFFFFF\" fill-rule=\"nonzero\"> <g id=\"noun_466311_cc-copy\" transform=\"translate(649.500000, 728.500000) scale(-1, 1) rotate(-90.000000) translate(-649.500000, -728.500000) translate(641.000000, 671.000000)\"> <polygon id=\"Shape\" points=\"14.0533134 104.213608 9.35128014 109.129313 9.35128014 88.5597291 6.64871986 88.5597291 6.64871986 109.129313 1.94668659 104.213608 0 106.100858 8 114.464491 16 106.100858\"></polygon> </g> </g> </g></svg></div>";
+
         var arrowRight = "<div class='arrow right'><svg width=\"27px\" height=\"17px\" viewBox=\"0 0 27 17\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\"> <g id=\"Page-1\" stroke=\"none\" stroke-width=\"1\" fill=\"none\" fill-rule=\"evenodd\"> <g id=\"Pre-fin-Copy-2\" transform=\"translate(-680.000000, -720.000000)\" fill=\"#FFFFFF\" fill-rule=\"nonzero\"> <g id=\"noun_466311_cc-copy\" transform=\"translate(649.500000, 728.500000) scale(-1, 1) rotate(-90.000000) translate(-649.500000, -728.500000) translate(641.000000, 671.000000)\"> <polygon id=\"Shape-Copy\" transform=\"translate(8.249795, 13.297414) scale(-1, 1) rotate(-180.000000) translate(-8.249795, -13.297414) \" points=\"14.3031081 15.9989117 9.60107486 20.9146165 9.60107486 0.345032821 6.89851459 0.345032821 6.89851459 20.9146165 2.19648132 15.9989117 0.249794726 17.8861615 8.24979473 26.2497947 16.2497947 17.8861615\"></polygon> </g> </g> </g></svg></div>";
+
+        // ========== COLOR THIEF ==========
+
+        var colorHeader;
+        var colorSlider = $('.slider-color');
+
+        var colorSet = function () {
+            var imgSrc = $('.slider-color .slick-slide.slick-current.slick-active .img-color').attr('src');
+            var imgObj = new Image();
+
+            imgObj.src = imgSrc;
+
+            var colorThief = new ColorThief();
+            colorHeader = colorThief.getColor(imgObj);
+        }
+
+        colorSlider.on('init', colorSet);
+
+        colorSlider.slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            prevArrow: arrowLeft,
+            nextArrow: arrowRight,
+            fade: true,
+            speed: 0
+        }).on('afterChange', colorSet);
+
+        $('.link-color').hover(function () {
+            $('header').css('background-color', 'rgb(' + colorHeader + ')')
+        }, function () {
+            $('header').css('background-color', '#a0b3c9')
+        });
 
         // ----- SLICK SLIDER COUNTER -----
         // $('.slider-full').on('init reInit afterChange', function (event, slick, currentSlide) {
@@ -52,11 +92,7 @@
         // });
 
         // ----- SLICK SLIDER -----
-        // $(".slider-full").slick({
-        //     prevArrow: "<div class='arrow left'><i class='fa fa-angle-left'></i></div>",
-        //     nextArrow: "<div class='arrow right'><i class='fa fa-angle-right'></i></div>",
-        //     dots: true
-        // });
+
 
         // $(".slider-two").slick({
         //     dots: true,
@@ -127,7 +163,6 @@
             initialSlide: 2,
             centerMode: true,
             asNavFor: '.slider-rows-sync',
-
         });
 
         $(".slider-rows-sync").slick({
@@ -149,6 +184,28 @@
 
         // ========== TABS ==========
         // $('#tabs').rewTabs();
+
+        (function () {
+            var tabsArr = $('.tabs-publication--ul li a');
+            var tabsSlider = $('.tabs-publication--line');
+
+            $(tabsArr).click(function (e) {
+                e.preventDefault();
+
+                var tabsIndex = $(tabsArr).index(this);
+                var tabsLeft = 0;
+
+                for (var i = 0; i < tabsIndex; i++) {
+                    tabsLeft += $(tabsArr[i]).parent().outerWidth(true);
+                }
+
+                $(tabsArr).removeClass('active');
+                $(this).addClass('active');
+                $(tabsSlider).css('transform', 'translate3d(' + (tabsLeft - 8) + 'px,0,0)');
+
+            });
+        })();
+
 
         // ========== DATEPICKER ==========
         // $(".datepicker").flatpickr();
